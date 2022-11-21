6187b2343832f769094dd4bef745cf9594d437dc
quardianwolf <quardianwolf@gmail.com>
2022-11-22 01:47:33 +0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '../styles/globals.css'
+import '../styles/styles.scss'
+import { Layout, } from '../components';
+import { Navbar, Footer } from '../structure';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return
+  <Layout>
+    <Navbar />
+    <Component {...pageProps} />
+    <Footer />
+  </Layout>
 }
